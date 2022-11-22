ae8217d707a3545954001e554fe6410329132671
Kennysaruni <kenny.saruni@student.moringaschool.com>
2022-11-22 12:20:08 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,28 @@
+function add(x,y){
+    return x+y
+}
+function subtract(x,y){
+    return x-y
+}
+function multiply(x,y){
+    return x*y
+}
+function divide(x,y){
+    return x/y
+}
+
+function increment(n){
+    n++;
+    return n;
+}
+function decrement(n){
+    n--;
+    return n;
+}
+function makeInt(n){
+    parseInt("n",10)
+    return n;
+}
+function preserveDecimal(n){
+    return n;
+}
